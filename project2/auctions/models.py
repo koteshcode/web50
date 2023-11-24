c5c44355696972ffb0653084472cb2b45efe0fca
koteshcode <kot3nb3rg@gmail.com>
2023-11-24 07:26:22 +0100

    Add project2

---
+++ project2/auctions/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Item(models.Model):
+    title = models.CharField(max_length=64)
+    image_url = models.URLField(blank=True)
+    description = models.TextField(max_length=200)
+    category = models.CharField(max_length=16, blank=True)
+    bid = models.DecimalField(decimal_places=2, max_digits=10)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=128)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
