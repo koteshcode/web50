a8c4b1d675b89437094eedfb0ee0475500ea9f85
koteshcode <kot3nb3rg@gmail.com>
2023-12-14 23:01:10 +0100

    Fix db and bootsrtap version

--- project4/network/views.py
+++ project4/network/views.py
@@ -61,3 +61,6 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+def post_message(request):
+    return render(request, "network/index.html")
